ae144ee601d16cf6ec0e3e23e93d0a327442b713
Yannick Severo <severoyannick@gmail.com>
2021-09-17 12:24:39 +0200

    Tant que la valeur saisie n'est pas la bonne, on lui demande à nouveau

--- js/main.js
+++ js/main.js
@@ -5,8 +5,23 @@ var randomNumber = Math.round(Math.random() * 500);
 // la reponse en console
 console.log("Random number : ", randomNumber);
 
-// On demande à l'utilisateur de saisir un nombre
-var response = prompt("Devine le juste prix");
-// On transforme sa saisie en nombre
-var responseInteger = parseInt(response, 10);
-console.log(responseInteger);
+
+// Tant que la valeur saisie n'est pas la bonne, on lui demande à nouveau
+
+do {
+  // On demande à l'utilisateur de saisir un nombre
+  var response = prompt("Devine le juste prix");
+  
+  // On transforme sa saisie en nombre
+  var responseInteger = parseInt(response, 10);
+  console.log(responseInteger);
+
+  // Tant que la valeur saisie n'est pas la bonne, on lui demande à nouveau
+  if (randomNumber != responseInteger) {
+    var response = prompt("Devine le juste prix");
+  }
+  
+  
+} while (randomNumber != responseInteger);
+
+alert("C'est gagné ! 🎉");
