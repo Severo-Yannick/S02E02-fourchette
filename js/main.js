9720964028706b5736dd33e5dd42d18ef40ff84a
Yannick Severo <severoyannick@gmail.com>
2021-09-17 12:30:00 +0200

    Si la saisie est trop petite on pourrait l'indiquer

--- js/main.js
+++ js/main.js
@@ -5,23 +5,17 @@ var randomNumber = Math.round(Math.random() * 500);
 // la reponse en console
 console.log("Random number : ", randomNumber);
 
-
 // Tant que la valeur saisie n'est pas la bonne, on lui demande à nouveau
-
 do {
   // On demande à l'utilisateur de saisir un nombre
   var response = prompt("Devine le juste prix");
-  
   // On transforme sa saisie en nombre
   var responseInteger = parseInt(response, 10);
   console.log(responseInteger);
-
-  // Tant que la valeur saisie n'est pas la bonne, on lui demande à nouveau
-  if (randomNumber != responseInteger) {
-    var response = prompt("Devine le juste prix");
+  // Si la saisie est trop petite on pourrait l'indiquer
+  if (randomNumber < responseInteger) {
+    alert('C\'est moins')
   }
-  
-  
 } while (randomNumber != responseInteger);
 
 alert("C'est gagné ! 🎉");
