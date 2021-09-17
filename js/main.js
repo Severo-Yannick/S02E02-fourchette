918045397a8150b599be01de4481893a5f0ddc5a
Yannick Severo <severoyannick@gmail.com>
2021-09-17 12:42:43 +0200

    Imposer à l'utilisateur d'entrer un nombre

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 console.log("Let's go !");
 
 var randomNumber = Math.round(Math.random() * 500);
-
+var score = 1;
 // la reponse en console
 console.log("Random number : ", randomNumber);
 
@@ -12,14 +12,21 @@ do {
   // On transforme sa saisie en nombre
   var responseInteger = parseInt(response, 10);
   console.log(responseInteger);
+
+  // Obliger l'utilisateur à entrer un nombre
+  if (isNaN(responseInteger)) {
+    alert('Merci de saisir un nombre')
+  }
   // Si la saisie est trop petite on pourrait l'indiquer
-  if (randomNumber < responseInteger) {
+  else if (randomNumber < responseInteger) {
+    score++
     alert('C\'est moins')
   }
   // Sinon on pourrait dire que c'est trop grand
-  if (randomNumber > responseInteger) {
+  else if (randomNumber > responseInteger) {
+    score++
     alert('C\'est plus')
   }
 } while (randomNumber != responseInteger);
 
-alert("C'est gagné ! 🎉");
+alert("C'est gagné ! 🎉 - Nombre d'essais " + score);
