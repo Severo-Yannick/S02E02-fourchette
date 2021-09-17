4974df5e791be6680dcb5658b2e65525b6ba1a8b
Yannick Severo <severoyannick@gmail.com>
2021-09-17 12:08:11 +0200

    On pense à transformer sa saisie en nombre

--- js/main.js
+++ js/main.js
@@ -7,4 +7,6 @@ console.log("Random number : ", randomNumber);
 
 // On demande à l'utilisateur de saisir un nombre
 var response = prompt("Devine le juste prix");
-console.log(response);
+// On transforme sa saisie en nombre
+var responseInteger = parseInt(response, 10);
+console.log(responseInteger);
