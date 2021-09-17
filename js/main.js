fbb80f37d3692579b6b3b3eade344fda0613746c
Yannick Severo <severoyannick@gmail.com>
2021-09-17 12:05:12 +0200

    On demande à l'utilisateur de saisir un nombre

--- js/main.js
+++ js/main.js
@@ -4,3 +4,7 @@ var randomNumber = Math.round(Math.random() * 500);
 
 // la reponse en console
 console.log("Random number : ", randomNumber);
+
+// On demande à l'utilisateur de saisir un nombre
+var response = prompt("Devine le juste prix");
+console.log(response);
